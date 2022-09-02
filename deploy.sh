93d369010b39fa376557c32997a3ad81dd43044c
Dzevad <dzevad.del@gmail.com>
2022-09-02 20:04:12 +0200

    last commit

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:lulzz/lulzz.github.io.git main
+# git push -f git@github.com:lulzz/lulzz.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:lulzz/frontendmentor-product-prev-card.git main:gh-pages
+git push -f git@github.com:lulzz/frontendmentor-product-prev-card.git main:gh-pages
 
 cd -
